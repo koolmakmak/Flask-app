81807ea24f5f2d1643638c0dcfed2e3212de9f5c
koolmakmak <66991041@kmitl.ac.th>
2025-10-01 12:38:14 +0700

    Add files via upload

--- AI/main.py
+++ AI/main.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-from plant import bp_plant
-from soil import bp_soil
-from sensor import bp_sensor
+from .plant import bp_plant
+from .soil import bp_soil
+from .sensor import bp_sensor
 
 app = Flask(__name__)
 CORS(app)
